7d118e367f1723353ceec47754fffc06a9f63f3d
Andrew C. Dvorak <acdvorak@gmail.com>
2026-03-22 15:40:34 -0500

    Implement stable display instance identifiers

--- src/cpp/DisplayProberInternal.h
+++ src/cpp/DisplayProberInternal.h
@@ -17,13 +17,24 @@ std::string TryToExtractShortLivedIdentifier(std::string_view input);
 
 std::string TryToExtractEdid7DigitIdentifier(std::string_view input);
 
+void ParseEdidStrings(const std::vector<std::uint8_t>& edid_bytes,
+                      json::WinDisplay& display);
+
 std::string BuildPrimaryPortKey(const ccd::CcdDisplayConfig& display);
 
 std::string BuildMonitorPathKey(const DevicePath& monitor_device_path);
 
-std::string BuildEdidKey(const std::optional<json::WinEdidInfo>& edid_info);
+std::string BuildLocationPortKey(const json::WinDisplay& json_obj);
 
-void PopulateStableKeyFields(json::WinDisplay& json_obj);
+std::string BuildEdidParsedKey(
+    const std::optional<json::WinEdidInfo>& edid_info,
+    const std::optional<std::string>& monitor_serial_string);
+
+std::string BuildEdidHashKey(const std::vector<std::uint8_t>& edid_bytes);
+
+void PopulateStableKeyFields(
+    json::WinDisplay& json_obj, const std::optional<std::string>& edid_hash_key,
+    const std::optional<std::string>& location_port_key);
 
 bool RectHasZeroWidthOrHeight(std::optional<RECT> rect);
 
